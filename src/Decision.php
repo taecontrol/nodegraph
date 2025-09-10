072c1c4b46a5c8238c6f7b84c4c789a78ee75acc
Luis Güette <guetteluis@gmail.com>
2025-09-10 17:02:21 -0400

    Add events handling to Decision class and create Event class

--- src/Decision.php
+++ src/Decision.php
@@ -4,7 +4,15 @@ namespace Taecontrol\NodeGraph;
 
 abstract class Decision implements Contracts\HasMetadata
 {
+    /**
+     * @var array <string, mixed> Metadata associated with the decision.
+     */
     protected array $metadata = [];
+
+    /**
+     * @var array<int, Event> List of event class associated with the decision.
+     */
+    protected array $events = [];
 
     public function metadata(): array
     {
@@ -19,5 +27,33 @@ abstract class Decision implements Contracts\HasMetadata
     public function addMetadata(string $key, mixed $value): void
     {
         $this->metadata[$key] = $value;
+    }
+
+    /**
+     * Get the list of event class associated with the decision.
+     *
+     * @return array<int, Event>
+     */
+    public function events(): array
+    {
+        return $this->events;
+    }
+
+    /**
+     * Set the list of event class associated with the decision.
+     *
+     * @param  array<int, Event>  $events
+     */
+    public function setEvents(array $events): void
+    {
+        $this->events = $events;
+    }
+
+    /**
+     * Add an event class to the decision.
+     */
+    public function addEvent(Event $event): void
+    {
+        $this->events[] = $event;
     }
 }
---
+++ src/Event.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Taecontrol\NodeGraph;
+
+abstract class Event
+{
+
+}
