d39291af72a217452619ee020c0a30f972266e63
Luis Güette <guetteluis@gmail.com>
2025-09-09 20:06:53 -0400

    Fix formatting in Node interface documentation

--- src/Contracts/Node.php
+++ src/Contracts/Node.php
@@ -6,14 +6,13 @@ namespace Taecontrol\NodeGraph\Contracts;
  * Interface Node
  *
  * @template TData
- * @package Taecontrol\NodeGraph\Contracts
  */
 interface Node
 {
     /**
      * Handle the given data.
      *
-     * @param TData $data
+     * @param  TData  $data
      * @return TData
      */
     public function handle($data);
