b351f90ef2e6e3ee7e301b414d789c9438baff5e
Luis Güette <guetteluis@gmail.com>
2025-09-11 17:00:14 -0400

    Refactor NodeGraphServiceProvider to simplify package configuration

--- src/NodeGraphServiceProvider.php
+++ src/NodeGraphServiceProvider.php
@@ -4,22 +4,14 @@ namespace Taecontrol\NodeGraph;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Taecontrol\NodeGraph\Commands\NodeGraphCommand;
 
 class NodeGraphServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('nodegraph')
             ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_nodegraph_table')
-            ->hasCommand(NodeGraphCommand::class);
+            ->hasMigration('create_nodegraph_table');
     }
 }
